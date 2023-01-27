e1af780712bf613d621f156bd6898d6d0d4b548c
MaeFire <masked_phoenix@yahoo.com>
2023-01-27 03:20:21 -0600

    Add win condition to game
    Defined what the win condition should be, and had it changed to
    reflect which player won.

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-line = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 # begin Game
 class Game
   attr_accessor :board
@@ -6,14 +5,17 @@ class Game
   # include GameLogic
   def initialize(player_one, player_two)
     @board = Array.new(10)
-    @players = [player_one.new(self, "X"), player_two.new(self, "O")]
+    @players = [player_one.new(self, 'X', 'Player One'),
+                player_two.new(self, 'O', 'Player Two')]
+    @line = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9],
+             [1, 5, 9], [3, 5, 7]]
   end
 
   # Here, we assign no values, to compare later. #
   def print_board
     (1..9).each_slice(3) do |row|
-      puts row.map { |cell| @board[cell] ? @board[cell] : cell }.join(" | ")
-      puts  "--+---+--"
+      puts row.map { |cell| @board[cell] || cell }.join(' | ')
+      puts '--+---+--'
     end
   end
 
@@ -27,13 +29,17 @@ class Game
     loop do
       print_board
       @current_player.place_token
+      return puts "#{@current_player.name} wins!" if check_win
+
       @current_player = @players.rotate!.first
-      return if check_win || check_tie
     end
   end
 
   def check_win
-    # check win here
+    @line.each do |winning_line|
+      return true if winning_line.all? { |cell| @board[cell] == @current_player.token }
+    end
+    false
   end
 
   def check_tie
@@ -44,11 +50,12 @@ end
 # this gives players the X or O tokens, and access to the game class,
 # due to above "self" argument
 class Player
-  def initialize(game_class, token)
+  def initialize(game_class, token, name)
     @game = game_class
     @token = token
+    @name = name
   end
-  attr_reader :token
+  attr_reader :token, :name
 end
 
 # this class lets player choose token spot, updates board, then switches player?
@@ -58,7 +65,7 @@ class HumanPlayer < Player
       print "Choose a valid position(1-9) to place your #{token}:"
       chosen_spot = gets.to_i
       unless @game.spot_empty?(chosen_spot) && (1..9).include?(chosen_spot)
-        puts "Choice is not a valid position. Please try again."
+        puts 'Choice is not a valid position. Please try again.'
         next
       end
       @game.board[chosen_spot] = @token
@@ -73,4 +80,4 @@ end
 # Do I need multiple human classes?
 test = Game.new(HumanPlayer, HumanPlayer)
 test.game_loop
-puts "done"
+puts 'done'
