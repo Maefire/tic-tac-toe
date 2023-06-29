2d01aaf973ee8c531a674b269519f6e215ef9134
MaeFire <masked_phoenix@yahoo.com>
2023-06-28 19:59:40 -0500

    Change variable and add meth and change parameters
    Changed variable name for clarity, and added a new "get_name" method from
    the tic_tac_toe_game file, and changed parameters to use a default get_name
    if a name wasn't provided.

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,16 @@
 class Player
-  attr_reader :token, :name, :game
+  attr_reader :token, :name, :player_class
 
-  def initialize(game_class, token, name)
-    @game = game_class
+  def initialize(player_class, token, name = get_name)
+    @player_class = player_class
     @token = token
     @name = name
+  end
+
+  def get_name
+    unless player_class.instance_of?(HumanPlayer)
+      puts "Please insert your name:"
+      @name = gets.chomp
+    end
   end
 end
