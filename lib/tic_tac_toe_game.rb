7ef9eb5100c0a5f4f7fb46615c14e9a7c83d104a
MaeFire <masked_phoenix@yahoo.com>
2023-06-28 19:52:30 -0500

    Change file name and refactor players variable
    Changed the file name to be more inline with functionality. Player class
    was modified, so I changed the instance variable accordingly.

---
+++ lib/tic_tac_toe_game.rb
@@ -0,0 +1,58 @@
+require_relative "./player"
+require_relative "./human_player"
+
+class TicTacToeGame
+  attr_accessor :board, :current_player
+  attr_reader :line
+
+  # include GameLogic
+  def initialize(player_one, player_two)
+    @board = Array.new(10)
+    @players = [player_one.new(self, "X"),
+      player_two.new(self, "O")]
+    @line = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9],
+      [1, 5, 9], [3, 5, 7]]
+    @current_player = @players.first
+  end
+
+  # Here, we assign no values, to compare later. #
+  def print_board
+    (1..9).each_slice(3) do |row|
+      puts row.map { |cell| board[cell] || cell }.join(" | ")
+      puts "--+---+--"
+    end
+  end
+
+  def game_loop
+    loop do
+      system("clear") || system("clr")
+      print_board
+      current_player.place_token
+      if check_win
+        print_board
+        return puts "#{current_player.name} outwitted their opponent!"
+      end
+      if check_tie
+        print_board
+        return puts "Both opponents played perfectly, resulting in a tie."
+      end
+      @current_player = @players.rotate!.first
+    end
+  end
+
+  def check_win
+    line.each do |winning_line|
+      return true if winning_line.all? { |cell| board[cell] == current_player.token }
+    end
+    false
+  end
+
+  def check_tie
+    # if no spots == nil, return true, else false
+    true unless @board[1..9].include?(nil)
+  end
+
+  def spot_empty?(cell)
+    board[cell].nil?
+  end
+end
