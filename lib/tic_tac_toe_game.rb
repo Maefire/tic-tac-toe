c2ae23eab6bee50ad32a068ba965a4c652bcbe5b
MaeFire <masked_phoenix@yahoo.com>
2023-07-12 16:00:21 -0500

    Separate concerns and clean up code
    My predicate methods had puts messages in them, and one contained extra
    logic not related to its return value. To rectify this, I added a new
    method for the puts messages, and changed the print_board method to
    self clear, allowing me to remove the extra calls to it.

--- lib/tic_tac_toe_game.rb
+++ lib/tic_tac_toe_game.rb
@@ -16,6 +16,7 @@ class TicTacToeGame
 
   # Here, we assign no values, to compare later. #
   def print_board
+    system("clear") || system("clr")
     (1..9).each_slice(3) do |row|
       puts row.map { |cell| board[cell] || cell }.join(" | ")
       puts "--+---+--"
@@ -23,23 +24,25 @@ class TicTacToeGame
   end
 
   def game_loop
+    result = game_over?
     loop do
-      system("clear") || system("clr")
       print_board
       current_player.place_token(self)
-      break if game_over?
+      result = game_over?
+      break if result
       @current_player = @players.rotate!.first
     end
+    handle_game_result(result)
   end
 
   def game_over?
-    system("clear") || system("clr")
-    print_board
-    tie? || win?
+    return "tie" if tie?
+    return "win" if win?
+
+    false
   end
 
   def win?
-    puts "#{current_player.name} outwitted their opponent!"
     line.each do |winning_line|
       return true if winning_line.all? { |cell| board[cell] == current_player.token }
     end
@@ -47,12 +50,22 @@ class TicTacToeGame
   end
 
   def tie?
-    return false if @board[1..9].include?(nil)
-    puts "You both played perfectly, resulting in a tie."
-    true
+    return true unless @board[1..9].include?(nil)
+    false
   end
 
   def spot_empty?(cell)
     board[cell].nil?
+  end
+
+  def handle_game_result(result)
+    case result
+    when "win"
+      print_board
+      puts "#{current_player.name} outwitted their opponent!"
+    when "tie"
+      print_board
+      puts "You both played perfectly, resulting in a tie."
+    end
   end
 end
